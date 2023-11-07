cd0b9ac3e43be28e644812768a8e21f462bc0c4e
Abhishek Jain <abhishekjain1416@gmail.com>
2023-11-07 22:35:26 +0530

    Feat: Java Mail Sender: Send Email with Gmail SMTP

--- src/main/java/com/programmingtechie/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/programmingtechie/notificationservice/NotificationServiceApplication.java
@@ -1,5 +1,6 @@
 package com.programmingtechie.notificationservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,13 +11,23 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class NotificationServiceApplication {
 
+	@Autowired
+	EmailSenderService senderService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
 	@KafkaListener(topics = "notificationTopic")
 	public void handleNotifications(OrderPlacedEvent orderPlacedEvent) {
+
 		// Send out an email notification
+		senderService.sendEmail(
+			"",
+			"Order Placed Successfullly",
+			"Your order has been placed successfully with Order ID "+orderPlacedEvent.getOrderNumber()
+		);
+
 		log.info("Received notification for order - {}",orderPlacedEvent.getOrderNumber());
 	}
 }
